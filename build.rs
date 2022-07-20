cca067d6afc79ba8441ffc2faa52e1c4bc03cc34
Hellzbellz123 <Smith.j.taylor2001@gmail.com>
2022-07-20 14:58:03 -0700

    the build script works if you pass a target but idk if it works if you dont

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
 
@@ -26,10 +27,16 @@ fn copyassets() {
         output_path.to_str().unwrap()
     );
 
-    let input_path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets/*");
-    let output_path = Path::new(&output_path).join("assets/*");
-    let res = std::fs::copy(input_path, output_path);
-    println!("cargo:info={:#?}", res)
+    let input_path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets/");
+    let output_path = Path::new(&output_path).join("assets/");
+    copy(input_path, output_path).expect("couldnt copy files, maybe the source doesnt exist? {}");
+}
+
+fn compilewindowicons() {
+    let target = env::var("TARGET").unwrap();
+    if target.contains("windows") {
+        embed_resource::compile("assets/icons/windows/icon.rc");
+    }
 }
 
 fn get_output_path() -> PathBuf {
@@ -46,9 +53,49 @@ fn get_output_path() -> PathBuf {
     path
 }
 
-fn compilewindowicons() {
-    let target = env::var("TARGET").unwrap();
-    if target.contains("windows") {
-        embed_resource::compile("assets/icons/windows/icon.rc");
+pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from.as_ref()));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from.as_ref()).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        println!("process: {:?}", &working_path);
+
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if fs::metadata(&dest).is_err() {
+            println!(" mkdir: {:?}", dest);
+            fs::create_dir_all(&dest)?;
+        }
+
+        for entry in fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
+                        fs::copy(&path, &dest_path)?;
+                    }
+                    None => {
+                        println!("failed: {:?}", path);
+                    }
+                }
+            }
+        }
     }
+
+    Ok(())
 }
