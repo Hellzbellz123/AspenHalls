c35c5fe2aa64bf994f81db890f8e978279cceb77
Hellzbellz123 <31550512+Hellzbellz123@users.noreply.github.com>
2022-08-31 03:16:32 -0700

    map stuff

---
+++ assets/tilemaps/TopDown_by_deepnight.tsx
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="TopDown_by_deepnight" tilewidth="16" tileheight="16" tilecount="121" columns="11">
+ <image source="TopDown_by_deepnight.png" width="176" height="176"/>
+ <tile id="0">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="collisions" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ assets/tilemaps/finalbossblues-icons_full_16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="finalbossblues-icons_full_16" tilewidth="16" tileheight="16" tilecount="1024" columns="16">
+ <image source="finalbossblues-icons_full_16.png" width="256" height="1024"/>
+</tileset>
